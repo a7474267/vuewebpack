96fb1637ee20f4b92cd32d6644414d1b3f973344
a7474267 <a7474267@gmail.com>
2021-02-22 17:11:01 +0800

    fix public path

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vuewebpack/dist/',
+    assetsPublicPath: '/vuewebpack/',
 
     /**
      * Source Maps
